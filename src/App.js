a6ab9b73d986ada411dc35bdf8ffb1b5f6b3cfe3
Neeraj Singh Rajput <78468513+NeerajSingh007@users.noreply.github.com>
2022-06-06 16:57:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
-
+import {Route,Routes} from 'react-router-dom';
+import Home from "./pages/Home";
+import RequiredAuth from "./hoc/RequiredAuth";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar/Navbar";
+import Product from "./components/Products/Product/Product";
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      
+      <Routes>
+        <Route path="/" element={<RequiredAuth><Home/></RequiredAuth>} ></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+      <Home/>
       {
         // Code here
       }
